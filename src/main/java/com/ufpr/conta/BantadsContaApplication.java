2664b03c832c499d83318e888456edfcc3fc2006
IamThiago-IT <thiagodossantos315@gmail.com>
2024-06-20 17:06:46 -0300

    feat: add files

---
+++ src/main/java/com/ufpr/conta/BantadsContaApplication.java
@@ -0,0 +1,13 @@
+package com.ufpr.conta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BantadsContaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BantadsContaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ufpr/conta/BantadsContaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufpr.conta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BantadsContaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
